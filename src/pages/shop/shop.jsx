3f8272ab523cea5d2933f62a24c212dc5e96b8e0
Yagmur Yigit <111638728+YagmurY90@users.noreply.github.com>
2025-07-06 05:45:58 +0300

    key atanarak hata çözüldü

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { PRODUCTS } from "../../products"; // ürünleri aldık
+import { Product } from "./product"; // bileşeni aldık
 
 export const Shop = () => {
   return (
@@ -7,8 +8,12 @@ export const Shop = () => {
       <div className="shopTitle">
         <h1>Douby Shop</h1>
       </div>
-      <div classname="products"></div>
 
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product key={product.id} data={product} />
+        ))}
+      </div>
     </div>
   );
 };
