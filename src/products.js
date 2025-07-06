ca1516658cbd9f855fbf2965ca5d4c2f78472ed6
Yagmur Yigit <111638728+YagmurY90@users.noreply.github.com>
2025-07-06 06:16:31 +0300

    örnek ürün verisi arttırıldı

--- src/products.js
+++ src/products.js
@@ -1,4 +1,15 @@
 import product1 from "./assets/Örnek.jpg"
+import product2 from "./assets/Örnek.jpg"
+import product3 from "./assets/Örnek.jpg"
+import product4 from "./assets/Örnek.jpg"
+import product5 from "./assets/Örnek.jpg"
+import product6 from "./assets/Örnek.jpg"
+import product7 from "./assets/Örnek.jpg"
+import product8 from "./assets/Örnek.jpg"
+import product9 from "./assets/Örnek.jpg"
+import product10 from "./assets/Örnek.jpg"
+import product11 from "./assets/Örnek.jpg"
+import product12 from "./assets/Örnek.jpg"
 export const PRODUCTS = 
 [
   {
@@ -6,6 +17,72 @@ export const PRODUCTS =
     productName:"XXX",
     price:999.9,
     productImage:product1,
+  },
+  {
+    id: 2,
+    productName:"XXX",
+    price:999.9,
+    productImage:product2,
+  },
+  {
+    id: 3,
+    productName:"XXX",
+    price:999.9,
+    productImage:product3,
+  },
+  {
+    id: 4,
+    productName:"XXX",
+    price:999.9,
+    productImage:product4,
+  },
+  {
+    id: 5,
+    productName:"XXX",
+    price:999.9,
+    productImage:product5,
+  }, 
+  {
+    id: 6,
+    productName:"XXX",
+    price:999.9,
+    productImage:product6,
+  },
+  {
+    id: 7,
+    productName:"XXX",
+    price:999.9,
+    productImage:product7,
+  },
+  {
+    id: 8,
+    productName:"XXX",
+    price:999.9,
+    productImage:product8,
+  },
+    {
+    id: 9,
+    productName:"XXX",
+    price:999.9,
+    productImage:product9,
+  }, 
+  {
+    id: 10,
+    productName:"XXX",
+    price:999.9,
+    productImage:product10,
+  },
+  {
+    id: 11,
+    productName:"XXX",
+    price:999.9,
+    productImage:product11,
+  },
+  {
+    id: 12,
+    productName:"XXX",
+    price:999.9,
+    productImage:product12,
   }
   
 
