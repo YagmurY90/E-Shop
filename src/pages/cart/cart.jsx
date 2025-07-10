62bd0ce9969cf5d7018038c3de8756480ac07736
Yagmur Yigit <111638728+YagmurY90@users.noreply.github.com>
2025-07-11 02:52:40 +0300

    Sepetteki ürünler gösterilmeye başlandı

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop_context";
+import { PRODUCTS } from "../../products";
+import { CartItem } from "./cart_item";
+
 export const Cart = () => {
+  const { cartItems } = useContext(ShopContext);
+
   return (
-    <div>caaart</div>
-  )
-}
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cart">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
+    </div>
+  );
+};
