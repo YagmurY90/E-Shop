70f4e5907624d26e45592d221c4f72c2ef6d334f
Yagmur Yigit <111638728+YagmurY90@users.noreply.github.com>
2025-07-11 03:00:05 +0300

    cart.css import edildi

---
+++ src/pages/cart/cart_item.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop_context";
+
+export const CartItem = (props) => {
+  const { id, productName, price, productImage } = props.data;
+  const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
+    useContext(ShopContext);
+
+  return (
+    <div className="cartItem">
+      <img src={productImage} />
+      <div className="description">
+        <p>
+          <b>{productName}</b>
+        </p>
+        <p> Price: ${price}</p>
+      </div>
+    </div>
+  );
+};
