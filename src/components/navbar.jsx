577bc6b900559bcf368b9f3e7bbdccaeb1fc4a32
Yagmur Yigit <111638728+YagmurY90@users.noreply.github.com>
2025-07-12 06:38:30 +0300

    linkler etkileşimli hale getirildi

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
+import { ShopContext } from "../context/shop_context";
 import "./navbar.css";
 
 export const Navbar = () => {
+  const { cartItems, favorites } = useContext(ShopContext);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const totalCartCount = Object.values(cartItems).reduce((a, b) => a + b, 0);
+
   return (
     <div className="navbar">
-      <div className="links">
-        <Link to="/"> Shop </Link>
-        <Link to="/cart">
-          <ShoppingCart size={32} />
+      <div className="navbar-left">
+        <Link to="/" className="logo">Douby</Link>
+      </div>
+
+      <div className={`navbar-links ${menuOpen ? "active" : ""}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Shop</Link>
+        <Link to="/favorites" onClick={() => setMenuOpen(false)}>
+          ❤️ {favorites.length > 0 && <span className="badge">{favorites.length}</span>}
         </Link>
+        <Link to="/cart" onClick={() => setMenuOpen(false)}>
+          <ShoppingCart size={22} />
+          {totalCartCount > 0 && <span className="badge">{totalCartCount}</span>}
+        </Link>
+      </div>
+
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
       </div>
     </div>
   );
