d3baa1ab2ebe76b353fdc66dd1eccdaf8bb181f0
Yagmur Yigit <111638728+YagmurY90@users.noreply.github.com>
2025-07-20 20:29:31 +0300

    yeni örnek ürünler eklendi

--- src/products.js
+++ src/products.js
@@ -10,6 +10,19 @@ import product9 from "./assets/Örnek.jpg"
 import product10 from "./assets/Örnek.jpg"
 import product11 from "./assets/Örnek.jpg"
 import product12 from "./assets/Örnek.jpg"
+import product13 from "./assets/Örnek.jpg"
+import product14 from "./assets/Örnek.jpg"
+import product15 from "./assets/Örnek.jpg"
+import product16 from "./assets/Örnek.jpg"
+import product17 from "./assets/Örnek.jpg"
+import product18 from "./assets/Örnek.jpg"
+import product19 from "./assets/Örnek.jpg"
+import product20 from "./assets/Örnek.jpg"
+import product21 from "./assets/Örnek.jpg"
+import product22 from "./assets/Örnek.jpg"
+import product23 from "./assets/Örnek.jpg"
+import product24 from "./assets/Örnek.jpg"
+
 export const PRODUCTS = 
 [
   {
@@ -83,7 +96,79 @@ export const PRODUCTS =
     productName:"XXX",
     price:999.9,
     productImage:product12,
-  }
+  },
+  {
+    id: 13,
+    productName:"XXX",
+    price:999.9,
+    productImage:product13,
+  },
+  {
+    id: 14,
+    productName:"XXX",
+    price:999.9,
+    productImage:product14,
+  },
+  {
+    id: 15,
+    productName:"XXX",
+    price:999.9,
+    productImage:product15,
+  },
+  {
+    id: 16,
+    productName:"XXX",
+    price:999.9,
+    productImage:product16,
+  },
+  {
+    id: 17,
+    productName:"XXX",
+    price:999.9,
+    productImage:product17,
+  }, 
+  {
+    id: 18,
+    productName:"XXX",
+    price:999.9,
+    productImage:product18,
+  },
+  {
+    id: 19,
+    productName:"XXX",
+    price:999.9,
+    productImage:product19,
+  },
+  {
+    id: 20,
+    productName:"XXX",
+    price:999.9,
+    productImage:product20,
+  },
+    {
+    id: 21,
+    productName:"XXX",
+    price:999.9,
+    productImage:product21,
+  }, 
+  {
+    id: 22,
+    productName:"XXX",
+    price:999.9,
+    productImage:product22,
+  },
+  {
+    id: 23,
+    productName:"XXX",
+    price:999.9,
+    productImage:product23,
+  },
+  {
+    id: 24,
+    productName:"XXX",
+    price:999.9,
+    productImage:product24,
+  },
   
 
 
