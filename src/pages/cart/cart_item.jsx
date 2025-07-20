685163342e9e93e1450ae56716c761f563552652
Yagmur Yigit <111638728+YagmurY90@users.noreply.github.com>
2025-07-20 20:33:22 +0300

    ürünler için checkbox ile seçme özelliği ve sepetten ürün silme butonu eklendi

--- src/pages/cart/cart_item.jsx
+++ src/pages/cart/cart_item.jsx
@@ -1,19 +1,26 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../../context/shop_context";
+import { Trash } from "phosphor-react";
 
 export const CartItem = (props) => {
   const { id, productName, price, productImage } = props.data;
-  const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
-    useContext(ShopContext);
+  const {
+    cartItems,
+    addToCart,
+    removeFromCart,
+    updateCartItemCount,
+    removeItemFromCart,
+    selectedItems,
+    toggleSelectItem,
+  } = useContext(ShopContext);
 
   return (
     <div className="cartItem">
       <img src={productImage} />
+      
       <div className="description">
-        <p>
-          <b>{productName}</b>
-        </p>
-        <p> Price: ${price}</p>
+        <p><b>{productName}</b></p>
+        <p>Price: ${price}</p>
         <div className="countHandler">
           <button onClick={() => removeFromCart(id)}> - </button>
           <input
@@ -22,6 +29,22 @@ export const CartItem = (props) => {
           />
           <button onClick={() => addToCart(id)}> + </button>
         </div>
+      </div>
+
+      <div className="actions">
+        <input
+          type="checkbox"
+          checked={selectedItems.includes(id)}
+          onChange={() => toggleSelectItem(id)}
+          title="Bu ürünü seç"
+        />
+        <button
+          className="delete-btn"
+          onClick={() => removeItemFromCart(id)}
+          title="Ürünü sepetten kaldır"
+        >
+          <Trash size={20} />
+        </button>
       </div>
     </div>
   );
