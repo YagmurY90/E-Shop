7a6ad1af74119e4c66a08ce5a902e831e7d4ba33
Yagmur Yigit <111638728+YagmurY90@users.noreply.github.com>
2025-07-20 20:29:13 +0300

    kullanıcı girişinde username localStorage’a yazılıyor ve başarı mesajı gösteriliyor

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "./login.css";
+import { toast } from "react-hot-toast";
+
 
 const Login = () => {
   const { t } = useTranslation();
@@ -12,14 +14,19 @@ const Login = () => {
   const dummyUser = {
     email: "yagmurygt90@hotmail.com",
     password: "123456",
+    name: "Yağmur"
   };
 
   const handleLogin = (e) => {
     e.preventDefault();
-    if (email === dummyUser.email && password === dummyUser.password) {
-      localStorage.setItem("isAuth", "true");
-      navigate("/");
-    } else {
+   if (email === dummyUser.email && password === dummyUser.password) {
+  localStorage.setItem("isAuth", "true");
+  localStorage.setItem("username", dummyUser.name); // 👈 EKLENDİ
+  toast.success(t("login.success"));
+  setTimeout(() => {
+    window.location.href = "/";
+  }, 500);
+}else {
       alert(t("login.invalid"));
     }
   };
